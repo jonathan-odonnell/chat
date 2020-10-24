6c8ceeea366469981cf48689c8143dc845524fa5
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2020-10-24 22:10:03 +0000

    Fixed bug causing user who sent the message to not display

--- run.py
+++ run.py
@@ -11,7 +11,7 @@ messages = []
 def add_message(username, message):
     """Add messages to 'messages' list """
     now = datetime.now().strftime("%H:%M:%S")
-    messages.append({"timestamp": now, "username": username, "message": message})
+    messages.append({"timestamp": now, "from": username, "message": message})
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
